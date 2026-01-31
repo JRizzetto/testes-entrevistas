b57c0fd970672570abdeb157e2e686860fbb7803
JRizzetto <jeffersonrizzetto@gmail.com>
2026-01-31 00:04:42 -0300

    Exercícios: 3-consume-api

---
+++ javascript-e-manipulacao-dom/3-consume-api/index.js
@@ -0,0 +1,28 @@
+/*
+Challenge 3 — Fetch + Render Users (API + DOM)
+
+Interview context
+“You need to consume an API, show results in the UI, and handle loading + errors.”
+
+🎯 Goal
+
+Fetch a list of users from an API and render them into the DOM.
+
+✅ Use this API (free, no key):
+https://jsonplaceholder.typicode.com/users
+
+Requirements — JavaScript
+1 - When the page loads, show Loading...
+2 - Fetch users from the API using fetch
+3 - If success:
+- Render a list of users
+- Each item must show: Name, Email, City (from adress.city)
+4 - If error:
+- Show "Something went wrong. Try again."
+- Show a Retry button
+5 - Retry button must retech the data
+6 - Keep DOM updates clean (use render() style functions)
+
+Bonus (optional, but interview-friendly):
+- Add a search input to filter users by name (client-side)
+*/
